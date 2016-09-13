5e2d0cbf577d4ee3f45d4e5720f735e2afd1d828
Neela Krishna Teja Tadikonda <ntadiko@ncsu.edu>
2016-09-13 07:30:46 -0400

    dev0

--- Train.py
+++ Train.py
@@ -3,5 +3,32 @@
 
 class Train(Object):
     
-    def __init__(self):
-        pass
+    def __init__(self,cars,engine):
+        self.cars = cars
+        self.engine = engine
+
+class Seat(object):
+	"""docstring for Seat"""
+	def __init__(self, position, dimensions):
+		super(Seat, self).__init__()
+		self.position = position
+		self.dimensions = dimensions
+		
+class Cars(object):
+	"""docstring for Cars"""
+	def __init__(self, seats, dimensions):
+		super(Cars, self).__init__()
+		self.seats = seats
+		self.dimensions = dimensions
+		
+class Engine(object):
+	"""docstring for Engine"""
+	def __init__(self, power):
+		super(Engine, self).__init__()
+		self.power = power
+		
+class Rail(object):
+	"""docstring for Rail"""
+	def __init__(self, separation):
+		super(Rail, self).__init__()
+		self.separation = separation
