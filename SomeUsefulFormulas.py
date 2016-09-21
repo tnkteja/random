62d26d14ab5d644884003299a463e7c1b3745c3a
Neela Krishna Teja Tadikonda <tnkteja@users.noreply.github.com>
2016-09-20 22:44:59 -0400

    Update SomeUsefulFormulas.py

--- SomeUsefulFormulas.py
+++ SomeUsefulFormulas.py
@@ -7,3 +7,4 @@ sumoffirstNgeometricprogession = lambda x,y:  (x**(y+1) - 1)/(x-1) if x is not 1
 
 sigmaieq1tokiapowi = lambda x,y:  (x - ((y+1)*x**(y+1)) + (y*(x**(y+2))))/(1-x)**2 
 sigmaieqjtok1byiub = lambda x,y: math.loge(y/x-1)
+stirlingsformula = lambda x:  ((2*math.pi*x)**0.5)*(x/math.e)**x
