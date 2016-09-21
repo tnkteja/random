2423bcd24473bc67ba7181fc7198af1961d634f7
Neela Krishna Teja Tadikonda <tnkteja@users.noreply.github.com>
2016-09-20 22:20:10 -0400

    Create SomeUsefulFormulas.py

---
+++ SomeUsefulFormulas.py
@@ -0,0 +1,3 @@
+sumoffirstNnaturalnumbers = lambda x:  x*(x+1)/2
+sumofSquaresoffirstNnaturalnumbers = lambda x:  n*(n+1)((2*n)+1)/6
+sumoffirstNgeometricprogession = lambda x,y:  (x**(y+1) - 1)/(x-1) if x is not 1 else y+1
