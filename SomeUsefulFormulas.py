e6316587131d400641acd6b19cc1d1c3b9d45ea7
Neela Krishna Teja Tadikonda <tnkteja@users.noreply.github.com>
2016-09-20 22:53:34 -0400

    Update SomeUsefulFormulas.py

--- SomeUsefulFormulas.py
+++ SomeUsefulFormulas.py
@@ -1,9 +1,9 @@
 from __future__ import division
 import math
 
-sumoffirstNnaturalnumbers = lambda x:  x*(x+1)/2
+arthmeticsum = lambda x:  x*(x+1)/2
 sumofSquaresoffirstNnaturalnumbers = lambda x:  n*(n+1)((2*n)+1)/6
-sumoffirstNgeometricprogession = lambda x,y:  (x**(y+1) - 1)/(x-1) if x is not 1 else y+1
+geometricsum = lambda x,y:  (x**(y+1) - 1)/(x-1) if x is not 1 else y+1
 
 sigmaieq1tokiapowi = lambda x,y:  (x - ((y+1)*x**(y+1)) + (y*(x**(y+2))))/(1-x)**2 
 sigmaieqjtok1byiub = lambda x,y: math.loge(y/x-1)
