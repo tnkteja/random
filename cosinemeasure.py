3a5c5ee7965063be6156885bcc10e717aaccc9ac
Neela Krishna Teja Tadikonda <tnkteja@users.noreply.github.com>
2016-09-23 19:52:27 -0400

    Create cosinemeasure.py

---
+++ cosinemeasure.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+"""
+"""
+
+from __future__ import division
+import math
+
+class Vector(list):
+    def __abs__(self):
+         return sum([i**2 for i in self])**0.5
+    def __mul__(self,other):
+         return Vector([ v*other[i] for i,v in enumerate(other)])
+         
+cosinemeasure = lambda x,y:  (x*y)/(abs(x)*abs(y))
