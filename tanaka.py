b3e01cc033dff53ec5c8b37ebd5dde580514c128
Neela Krishna Teja Tadikonda <tnkteja@users.noreply.github.com>
2016-09-23 23:05:21 -0400

    Create tanaka.py

---
+++ tanaka.py
@@ -0,0 +1,7 @@
+f1 = lambda x1:  x1
+f2 = lambda x2:  x2
+import math
+g1 = lambda x1,x2:  -x1**2-x2**2+1+(0.1*math.cos(16*math.atanh(x1/x2)) <= 0
+g2 = (x1-0.5)**2 + (x2-0.5)**2 <= 0.5
+
+xbound = lamba x:  -math.pi <= x <= math.pi
