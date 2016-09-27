86cf2ee4013c273e75a01b25e3fc3c9b9a11eda8
Neela Krishna Teja Tadikonda <tnkteja@users.noreply.github.com>
2016-09-27 16:29:32 -0400

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -1,4 +1,27 @@
 #!/usr/bin/python
 
 def quicksort(arr):
-    pass
+    """
+    basic operations:
+    Running time analysis: 
+    Best case -
+    Average C
+    """
+    def hoare_partition(a,l,r):
+        pivot=a[r]
+        li=l
+        ri=r-1
+        while True:
+            while a[li]<pivot and li < ri:
+                li+=1
+            while a[ri]>=pivot and ri >= li:
+                ri-=1
+            if ri < li:
+                break
+            a[li],a[ri]=a[ri],a[li]
+        return pivot
+
+    if len(arr
+           
+           
+     
