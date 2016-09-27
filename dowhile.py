4d4db9326ee4fe59d7b82167acb656841e1a9e6d
Neela Krishna Teja Tadikonda <tnkteja@users.noreply.github.com>
2016-09-27 16:36:56 -0400

    Create dowhile.py

---
+++ dowhile.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+# do-while in python
+
+import random
+conditions = [random.random() < 0.5 for i in xrange(1,random.randint(0,26)]
+while True:
+    # do here
+    
+    if any(conditions):
+        break
+    
