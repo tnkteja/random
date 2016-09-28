255bd1705877c4e2ace7cd22a7fec26122176629
Neela Krishna Teja Tadikonda <tnkteja@users.noreply.github.com>
2016-09-27 21:16:39 -0400

    Update orthogonalcodes.py

--- orthogonalcodes.py
+++ orthogonalcodes.py
@@ -1,8 +1,14 @@
 from __future__ import division
 
-def dotproduct(X,Y):  return sum(map(lambda x,y:  x*y,X,Y)))/len(X)
+def dotproduct(X,Y):   return int(sum(map(lambda x,y:  x*y,X,Y))/len(X))
 A=(-1,-1,-1,1,1,-1,1,1)
-negation = lambda x:  [ 1 if i < -1 else 1 for i in X]
+negation = lambda X:  [ 1 if i < -1 else 1 for i in X]
 Ad = negation(A)
  
 print dotproduct(A,A)
+
+B=(-1,-1,1,-1,1,1,1,-1)
+
+print dotproduct(A,negation(B))
+print dotproduct(A,B)
+print dotproduct(A,negation(A))
