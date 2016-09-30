b4d315eb392f3952521d9c555c5264b4ba20de6e
Neela Krishna Teja Tadikonda <tnkteja@users.noreply.github.com>
2016-09-30 00:18:06 -0400

    Create Optimizers.py

---
+++ Optimizers.py
@@ -0,0 +1,26 @@
+class Optimizer(object):
+	"""
+	"""
+
+	def __init__(self):
+		self.name = self.__class__.__name__
+
+
+class MaxWalkSat(Optimizer):
+	"""
+	"""
+
+	def __init__(self):
+		super(MaxWalkSat, self).__init__()
+		
+	def run(self):
+
+		#Jump all around the hills
+		x1,x2,x3,x4,x5,x6 =  
+		#Sometimes, sitting still while rolling marbles left and right
+		#Then taking one step along the direction where the marbles roll the furthest.
+		#Go to 1.
+
+x=MaxWalkSat()
+print x.name
+
