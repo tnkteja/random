9ddbeb5ecee33b4273291e671d98152b68fa85a7
Neela Krishna Teja Tadikonda <tnkteja@users.noreply.github.com>
2016-10-02 19:37:31 -0400

    Update rawsocket-injection.py

--- rawsocket-injection.py
+++ rawsocket-injection.py
@@ -1,7 +1,11 @@
-import socket
+import socket, crcmod
+
+crc=crcmod.mkCrcFun(0x104c11db7,initCrc=0,xorOut=0xffffffff)
+print crc(buffer("helloworld"))
 
 s=socket.socket(socket.AF_PACKET,socket.SOCK_RAW)
 s.bind((interface, 0))
 
 # There goes plain hello world on wire 
 s.send("Hello World")
+
