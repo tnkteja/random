f9aa101a45fdf6f7f45254fd94396b46b86466f5
Neela Krishna Teja Tadikonda <tnkteja@users.noreply.github.com>
2016-10-07 02:54:59 -0400

    Create flow.py

---
+++ flow.py
@@ -0,0 +1,22 @@
+class pipe(object):
+    def __init__():
+        pass
+
+"""
+Some questions, what would you use if it a ethernet lan how many bridges will you use, or you will use switches that act as briges but
+buffer the the l2 packets for you.
+"""
+class path():
+    def __init__(self):
+        self.pipes=[] #(link,crosssection) - multiple vlans on one link, but not 
+        
+
+        
+
+
+"""
+why wuld I want to use l2 routing in after the core switch.
+    
+"""
+Characterising a flow.
+Each link in a datacenter is like a p
