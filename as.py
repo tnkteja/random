9546b7a79efb64067e57e4bffc5c02188c98002d
Neela Krishna Teja Tadikonda <tnkteja@users.noreply.github.com>
2016-10-26 23:37:03 -0400

    Create as.py

---
+++ as.py
@@ -0,0 +1,16 @@
+class as(object):
+  def __init__(self):
+      pass
+
+class multihomedas(as):
+  def __init__(self):
+      pass
+      
+class transitas(as):
+  def __init__(self):
+      pass
+      
+class ixp(as):
+  def __init__(self):
+      pass
+      
