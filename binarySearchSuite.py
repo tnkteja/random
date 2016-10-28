ee55b06dfa907df0b5a0d45257f07cee89d1d569
Neela Krishna Teja Tadikonda <tnkteja@users.noreply.github.com>
2016-10-28 02:29:32 -0400

    Update binarySearchSuite.py

--- binarySearchSuite.py
+++ binarySearchSuite.py
@@ -2,9 +2,7 @@
 
 def binarySearchSuite(iterable,item,left=lambda x,y:  x<=y, right=lambda x,y:  x>=y,equal=None, leftHanded=True, greedy=False, duplicates=False):
     def binarySearch(iterable,item,left=lambda x,y:  x<=y, right=lambda x,y:  x>=y,equal=None,leftHanded=True, greedy=False):
-        N=len(iterable)
-        l=0
-        r=N-1
+        l,r=0,len(iterable)-1
         middle=lambda m: m if leftHanded else lambda m:  m+1
         while r-l >1:
             m=middle((l+r)/2)
