7698cba122bf6cc41ed562f23d88947771fb2696
Neela Krishna Teja Tadikonda <tnkteja@users.noreply.github.com>
2016-10-28 01:44:47 -0400

    Update binarySearchSuite.py

--- binarySearchSuite.py
+++ binarySearchSuite.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python
 
-def binarySearchSuite(iterable,item,key=None, leftHanded=True, greedy=False, duplicates=False):
-    def binarySearch(iterable,item,key=None,leftHanded=True, greedy=False):
+def binarySearchSuite(iterable,item,left=lambda x,y:  x<=y, right=lambda x,y:  x>=y,equal=None, leftHanded=True, greedy=False, duplicates=False):
+    def binarySearch(iterable,item,left=lambda x,y:  x<=y, right=lambda x,y:  x>=y,equal=None,leftHanded=True, greedy=False):
         N=len(iterable)
         l=0
         r=N-1
         middle=lambda m: m if leftHanded else lambda m:  m+1
         while r-l >1:
             m=middle((l+r)/2)
-            if key(item,iterable[m]):
+            if left(item,iterable[m]):
                 if greedy:  return m
                 r=m
             else:
                 l=m+1
-        if key(item,iterable[l]):
+        if equal(item,iterable[l]):
             return l
         else:
             return None
     if duplicates:
-        l=binarySearch(iterable, item, key, leftHanded=True)
-        r=binarySearch(iterable, item, key, leftHanded=False)
+        l=binarySearch(iterable, item,left=left, right=right, equal=equal, leftHanded=True)
+        r=binarySearch(iterable, item,left=left, right=right, equal=equal, leftHanded=False)
         return (l,r)
-    return binarySearch(iterable, item, key, leftHanded=leftHanded,greedy=greedy)
+    return binarySearch(iterable, item,left=left, right=right, equal=equal, leftHanded=leftHanded,greedy=greedy)
