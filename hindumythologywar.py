b0d9603bce48b910ae5980a36fd95445bb54d847
Neela Krishna Teja Tadikonda <tnkteja@users.noreply.github.com>
2016-11-07 23:30:20 -0500

    Create hindumythologywar.py

---
+++ hindumythologywar.py
@@ -0,0 +1,23 @@
+class enugu(object):
+    def __init__(self):
+        pass
+        
+class ratham(object):
+    pass
+
+class horse(object):
+    pass
+    
+class kalibantlu(object):
+    pass
+    
+class pathi(object):
+    def __init__(self,ratham=ratham(),ennugu=enugu(),gurralu=[gurram(),gurram(),gurram()],kalibantlu=[kalibantu(),kalibantu(),kalibantu(),kalibantu(),kalibantu()]):
+        pass
+        
+class senamukhamu(object):
+    def __init__(self,patis=[pathi(),pathi(),pathi()]):
+        pass
+        
+class gullamu(object):
+     def __init__(self,
