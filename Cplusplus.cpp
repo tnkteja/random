306845259084b294898da09f6342f22d93766dd9
Neela Krishna Teja Tadikonda <tnkteja@users.noreply.github.com>
2017-01-04 23:49:45 -0500

    dev0

--- Cplusplus.cpp
+++ Cplusplus.cpp
@@ -1,6 +1,43 @@
 #include<stdio.h>
 #include<iostream>
+#include<malloc.h>
 
-void globalFunction(const int one, const char* two){
+int globalFunction(const int one, char* const two,const char* three){
+    /*
+     *
+     */
+    
     return 0;
+}
+
+typedef struct _name{
+    void* pointer;
+} Name;
+
+class className{
+    public:   
+}
+
+struct structName{
+    private:
+}
+
+int main(unsigned int argc, char** argv){
+    
+    int sA[10]={0,2,3};
+    
+    int sA2[10]{0,6,8};
+ 
+    
+    int* dynamicArray=(int*)malloc(10*sizeof(int));
+    for(int i=0;i<10;i++){
+        dynamicArray[i]=0;
+    }
+    
+    int * dA2 = new int[10]{0,6,6};
+    
+    
+    
+    // Deallocate or delete
+    
 }
