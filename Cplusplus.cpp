9f5a527ad6947ebd20d1b943a9c36ba3ff5e92e9
Neela Krishna Teja Tadikonda <tnkteja@users.noreply.github.com>
2017-01-05 22:12:23 -0500

    Update Cplusplus.cpp

--- Cplusplus.cpp
+++ Cplusplus.cpp
@@ -2,7 +2,7 @@
 #include<iostream>
 #include<malloc.h>
 
-int globalFunction(const int one, char* const two,const char* three){
+int function(const int one, char* const two,const char* three){
     /*
      *
      */
@@ -14,16 +14,92 @@ typedef struct _name{
     void* pointer;
 } Name;
 
-class className{
-    public:   
+class BaseClass{
+    
+    protected:
+      int prv1;
+    
+    public:
+      int puv1;
+       
+      virtual BaseClass& method(){
+          printf("If called from base ref or pointer or the base object or base ref or base pointer to derived object wothout verridden method");
+      }
+    
+      int method2() final {
+      }
+      
+      virtual ~BaseClass(){
+          
+      }
+}
+
+
+class AbstractBaseClass {
+    /*
+     * Cannot be instantiated should only deried and instantiated.
+     */
+    
+    protected:
+    int v1;
+    
+    public:
+      // pure virtual function or abstract function
+      virtual int method() = 0;
+    
+      virtual int method2() = 0 {
+         printf("Pure virtual function with a default implementation");
+      }
+      
+       virtual ~AbstractBaseClass(){
+       }
+}
+
+class IinterfaceBaseClass {
+    public:
+    virtual int method1() = 0;
+    virtual int method2() = 0;
+    
+    virtual ~Interfacebase(){
+    }
+}
+
+// UNRESOLVED: derived from two classes with methods having same signature.
+
+class DerivedClass: public BaseClass, virtual public AbstractBaseClass {
+
+    public:
+   // Covariant return tyes
+   DerivedClass& method() override {
+        printf("If called from a base Ref or base Pointer of a Derived object");
+    }
+   
+    int AbstractbaseClass::method() final{
+    }
+    
+    //using default implementation.
+    int method2(){
+        return AbstractBaseClass::method2();
+    }
+}
+
+class DerivedClass2: virtual public BaseClass, virtual public AbstractBaseClass {
+    
 }
 
 struct structName{
     private:
 }
 
+class GrandDerivedClass{
+}
+
 int main(unsigned int argc, char** argv){
     
+    int r=function(0,0,'\0'); \\ Early binding
+    int (fptr*)(const int, char* const, const char *) = function;
+    fptr(0,0,'\0');  \\ late bnding
+        
     int sA[10]={0,2,3};
     
     int sA2[10]{0,6,8};
@@ -34,10 +110,21 @@ int main(unsigned int argc, char** argv){
         dynamicArray[i]=0;
     }
     
-    int * dA2 = new int[10]{0,6,6};
-    
-    
+    int * dA2 = new int[10]{0,6,6};   
     
     // Deallocate or delete
     
+    BaseClass base();
+    DerivedClass derived(5);
+    
+    BaseClass& baseref= derived;
+    
+    BaseClass* baseptr=derived;
+    
+    baseref.method();
+    baseref.BaseClass.method();
+    
+    BaseClass* baseArr=new BaseClass*[10]{ &derived, &base};
+    \\ Unresolve the sizes of the derived cass would be different, and allocating ht earray would be disprportionalte.
+   
 }
